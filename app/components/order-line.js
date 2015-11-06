5742e2bc5a0eccbef2a1fe0563f10e33918ac7df
Garry Dufford <gdufford@gmail.com>
2015-11-06 13:45:39 -0700

    Feels like proper design, using observers to calc lines and order

--- app/components/order-line.js
+++ app/components/order-line.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   //--put properties in here, make sure to add .property() to end of function
   productCategories: ["Jewelry","Henna","Shoes","Books","Clothing","Gifts"],
-  calcTotal: function() {
-    return Number(this.get('line').get('quantity')) * Number(this.get('line').get('cost'));
-  }.property('line.cost','line.quantity')
-}); 
+  calcLineTotal: function() {
+    var lineTotal = Number(this.get('line').get('quantity')) * Number(this.get('line').get('cost'));
+    this.get('line').set('total', lineTotal);
+  }.observes('line.cost','line.quantity')
+});
--- app/controllers/order.js
+++ app/controllers/order.js
@@ -5,22 +5,20 @@ import Ember from 'ember';
 //-- This line was 'Ember.Controller.extend' and was NOT working, the resulting orderline wasn't showing --//
 export default Ember.ObjectController.extend({
   needs: "application",
-  orderTotal: function() {
+  calcOrderTotal: function() {
     var orderTotal = 0;
     var allorderlines = this.get('orderlines');
     allorderlines.forEach(function (orderline) {
-      var calcLineTotal = Number(orderline.get('quantity')) * Number(orderline.get('cost'));
-      orderline.set('total',calcLineTotal);
-      orderTotal += calcLineTotal;
+      orderTotal += orderline.get('total');
     });
-    return orderTotal;
-  }.property('orderlines.@each.total', 'orderlines.@each.quantity', 'orderlines.@each.cost'),
-  actions: {
+    this.set('orderTotal', orderTotal);
+  }.observes('orderlines.@each.total'),
+  actions: { 
     newOrderLine: function(){
       var orderLinesByID = this.model.get('orderlines').mapBy('id');
       var nextId = orderLinesByID.length === 0 ? 1 : Math.max(...orderLinesByID) + 1;
 
-      var newOrderLine = this.store.createRecord('orderline',{id: nextId, quantity: 3, cost: 2, tax: 0, total: 0});
+      var newOrderLine = this.store.createRecord('orderline',{id: nextId, quantity: 1, cost: 0, tax: 0, total: 0});
 
       this.store.find('order', 1).then(function(order) {
         newOrderLine.set('order', order);
